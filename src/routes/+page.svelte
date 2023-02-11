434784118b22cb564d0068fb478d4d27d46ade8a
ratherlongname <shivam.bansal@live.com>
2023-02-11 15:17:20 +0530

    Remake old site in SvelteKit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+<article>
+<h1>👋 Hi! I'm Shivam.</h1>
+<p>Welcome to my personal website.</p>
+<p>
+	Here's my
+	<a href="/resume">resume</a>, and you can contact me here —
+	<a href="mailto:shivam.bansal@live.com">email</a>,
+	<a href="https://www.linkedin.com/in/shivambl">linkedin</a>,
+	<a href="https://github.com/shivambl">github</a>.
+</p>
+</article>
+</main>
+
+<style>
+	main {
+		padding: 2rem 1rem 5rem; /* padding: top horizontal bottom */
+
+		font-family: avenir, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu,
+			Cantarell, 'Helvetica Neue', sans-serif;
+
+		display: flex;
+		justify-content: center;
+	}
+
+	article {
+		padding: 1rem 1.5rem 1rem; /* padding: top horizontal bottom */
+
+		/* max-width: 45rem; */
+		width: 42.5rem;
+
+		border: solid;
+		border-width: 1.5px;
+	}
+
+	a,
+	a:visited {
+		text-decoration: none;
+		color: blue;
+	}
+
+	p {
+		font-size: 17px;
+	}
+</style>
