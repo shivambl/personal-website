629e4568d86bed2f3909513d8f7cb0626f0a8e0d
Shivam Bansal <shivam.bansal@live.com>
2023-02-11 15:29:09 +0530

    Use SvelteKit (#1)
    * Move old website to a dir
    
    * Update gitignore for sveltekit
    
    * Scaffold empty sveltekit project
    
    * Use SvelteKit SSG
    
    * Remake old site in SvelteKit

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,21 @@
+<object data="/Shivam_Bansal_Resume.pdf" type="application/pdf" title="Download resume">
+	Download my resume:
+	<a href="/Shivam_Bansal_Resume.pdf">Shivam_Bansal_Resume.pdf</a>
+</object>
+
+<style type="text/css">
+	:global(html),
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		height: 100%;
+		overflow: hidden;
+	}
+
+	object {
+		width: 100%;
+		height: 100%;
+		border: 0;
+	}
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
