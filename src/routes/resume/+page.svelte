84ae2ecd4a39c7d79f33f8803fac3f116d28fe03
ratherlongname <shivam.bansal@live.com>
2023-02-19 03:49:53 +0530

    Fix resume download link for mobile

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,23 +1,5 @@
 <h1 class="text-4xl pb-2 font-bold text-center">Resume</h1>
 
 <object class="flex-1" data="/Shivam_Bansal_Resume.pdf" type="application/pdf" title="Download resume">
-	Download my resume:
-	<a href="/Shivam_Bansal_Resume.pdf">Shivam_Bansal_Resume.pdf</a>
+	<a class="text-center text-blue-700 underline underline-offset-[4.5px] decoration-[1.5px]" href="/Shivam_Bansal_Resume.pdf">Resume ↓</a>
 </object>
-
-<style type="text/css">
-	/* :global(html),
-	:global(body) {
-		margin: 0;
-		padding: 0;
-		border: 0;
-		height: 100%;
-		overflow: hidden;
-	}
-
-	object {
-		width: 100%;
-		height: 100%;
-		border: 0;
-	} */
-</style>
