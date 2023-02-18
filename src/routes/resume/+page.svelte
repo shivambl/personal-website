43bfb0292d31d56eb0580e8de2ef834e3cb2a397
ratherlongname <shivam.bansal@live.com>
2023-02-19 03:53:15 +0530

    Center align resume download link

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,5 @@
 <h1 class="text-4xl pb-2 font-bold text-center">Resume</h1>
 
 <object class="flex-1" data="/Shivam_Bansal_Resume.pdf" type="application/pdf" title="Download resume">
-	<a class="text-center text-blue-700 underline underline-offset-[4.5px] decoration-[1.5px]" href="/Shivam_Bansal_Resume.pdf">Resume ↓</a>
+	<p class="text-center"><a class="text-blue-700 underline underline-offset-[4.5px] decoration-[1.5px]" href="/Shivam_Bansal_Resume.pdf">Download resume ↓</a></p>
 </object>
