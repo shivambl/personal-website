e2e54e8f614d9912702ea6557c881e9b1f044da8
ratherlongname <shivam.bansal@live.com>
2023-02-19 13:57:19 +0530

    Add work

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-4xl pb-2 font-bold text-center">Resume</h1>
+<h1 class="text-4xl pb-4 font-bold text-center">Resume</h1>
 
 <object class="flex-1" data="/Shivam_Bansal_Resume.pdf" type="application/pdf" title="Download resume">
 	<p class="text-center"><a class="text-blue-700 underline underline-offset-[4.5px] decoration-[1.5px]" href="/Shivam_Bansal_Resume.pdf">Download resume ↓</a></p>
