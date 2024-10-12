0623fab0d1e12f57b8a912ced3204559b2ee84ec
sbansal-eightfold <sbansal@eightfold.ai>
2024-10-13 03:17:40 +0530

    Add basic website

---
+++ src/components/Link.tsx
@@ -0,0 +1,17 @@
+const Link = ({ href='/', children='Link', isExternal = false }) => {
+    let attributes = {
+        className: 'underline underline-offset-[4.5px] text-orange-50 decoration-[1.5px] hover:text-orange-200',
+    }
+    if (isExternal) {
+        attributes['target'] = '_blank'
+        attributes['rel'] = 'noopener noreferrer'
+    }
+
+    return (
+        <a href={href} {...attributes}>
+            {children}{isExternal && ' ↗'}
+        </a>
+    )
+}
+
+export default Link
